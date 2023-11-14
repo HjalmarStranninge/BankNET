51bb57b2d4179c42d74a12cc85f50618ce07f355
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2023-11-14 12:30:56 +0100

    Added function for creating new account and transferring money between accounts.
    -Added new property 'Account Number' to Accounts.
    -Created 'BankHelpers' class containing 4 new methods.
    -Created CreateNewAccount in the UserFunctions class, which creates a new account, and a method with the same name in the  DbHelpers class which saves the account to the database.
    -Created TransferInternal methods in both UserFunctions and DbHelpers classes which transer money between two accounts on the same user.

--- BankNET/Program.cs
+++ BankNET/Program.cs
@@ -1,14 +1,34 @@
-﻿namespace BankNET
+﻿using BankNET.Data;
+using BankNET.Models;
+using BankNET.Utilities;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+
+namespace BankNET
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           // Add welcome-screen
+            // Add welcome-screen
 
-           // Add login interface. Logging out from an account should bring you back here.
+            // Add login interface. Logging out from an account should bring you back here.
 
-           // Add methods running either admin or user menus.
-        }
+            // Add methods running either admin or user menus.
+
+            using (var context = new BankContext())
+            {
+                var user = context.Users
+                    .Include(u => u.Accounts)
+                    .Where(u => u.Id == 1)
+                    .First();
+                    
+
+
+                
+                UserFunctions.TransferInternal(user);
+            };
+
+        } 
     }
 }
--- BankNET/Utilities/UserFunctions.cs
+++ BankNET/Utilities/UserFunctions.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using BankNET.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BankNET.Models;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace BankNET.Utilities
 {
@@ -11,13 +14,116 @@ namespace BankNET.Utilities
     {
         // Add method for viewing all user accounts with balance.
 
-        // Add method for moving money between users internal accounts.
+        // Add method for moving money between users internal accounts. - h
 
         // Add methods for deposit/withdraw.
 
-        // Add method for creating new account
-
         // Add method for transactions between separate users accounts. (Extra)
 
+        // Method for creating new accounts.
+        public static void CreateNewAccount(BankContext context, User user)
+        {
+            
+            Console.Clear();
+            Console.Write("Enter new account name: ");
+
+            string newAccountName = Console.ReadLine();
+                
+            // Generates a new account number and checks if it is unique. If it isn't, a new one is generated until it becomes unique.
+            string newAccountNumber;
+            do
+            {
+                newAccountNumber = BankHelpers.GenerateAccountNumber();
+
+            } while (!BankHelpers.IsAccountNumberUnique(newAccountNumber));
+
+            // Checks if the proposed name that the user entered isn't null. If it isn't, a new account is created.
+            if (!string.IsNullOrWhiteSpace(newAccountName))
+            {
+                DbHelpers.CreateNewAccount(context, newAccountName, newAccountNumber, user);
+
+                Console.Clear() ;
+                Console.WriteLine($"Successfully created new account with account name: {newAccountName}\n" +
+                    $"Your account number is {newAccountNumber}");
+
+                Thread.Sleep(1000);
+            }
+
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Account name cannot be empty");
+
+                Thread.Sleep(1000);
+            }
+            
+        }
+
+        // Method for transferring money internally between 2 accounts belonging to the same user.
+        public static void TransferInternal(User user)
+        {
+            Console.Clear();
+            Console.Write("Transfer to which account? \n");
+            string accountRecieving = Console.ReadLine();
+
+            Console.Write("Transfer from which account? \n");
+            string accountSending = Console.ReadLine();
+
+            // Checks if the accounts the user wants to transfer between exist.
+            if (BankHelpers.IsAccountNameMatching(accountRecieving, user) && BankHelpers.IsAccountNameMatching(accountSending, user))
+            {                            
+                decimal transferAmount = 0;
+                bool validInput = false;
+
+                // Keeps the user stuck in a loop until they enter a valid numeric amount.
+                do
+                {
+                    Console.Clear();
+                    Console.Write("Enter amount you wish to transfer: ");
+                    try
+                    {
+                        transferAmount = decimal.Parse(Console.ReadLine());
+                        validInput = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"An error occurred: {ex.Message}");
+                        Console.WriteLine("Please enter a valid amount, numbers only.");
+                        Thread.Sleep(2000);
+                    }
+
+                }while(!validInput);
+                
+                // Creating reference accounts for the sending and recieving accounts.
+                Account sendingAccount = user.Accounts.FirstOrDefault(account => account.AccountName.ToLower()  == accountSending.ToLower());
+                Account recievingAccount = user.Accounts.FirstOrDefault(account => account.AccountName.ToLower()  == accountRecieving.ToLower());
+
+                // Check if there is enough balance to complete the transaction and proceeds to do so if there is.
+                if(BankHelpers.IsThereBalance(sendingAccount, transferAmount))
+                {
+                    using(var context = new BankContext())
+                    {
+                        DbHelpers.TransferInternal(context, sendingAccount, recievingAccount, transferAmount);
+                    }
+                }
+                
+                // Error message if there isn't enough balance.
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Transaction failed. Not enough funds in the account.");
+                    Thread.Sleep(1000);
+                }
+            }
+
+            // Error message if the user enters an account name that doesn't match any existing account.
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Account names do not match. Transaction failed.");
+                Thread.Sleep(1000);
+            }
+        }
     }
 }
