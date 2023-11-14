b0d8640c5b8bb4b580b7e76d4c6d24f2ffb500f1
Charlotte <charlotteleyser@icloud.com>
2023-11-14 20:16:31 +0100

    Made user login and menu methods, also added welcome text in program.cs

--- BankNET/Program.cs
+++ BankNET/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace BankNET
+﻿using BankNET.Data;
+using BankNET.Utilities;
+using BankNET.Models;
+
+namespace BankNET
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           // Add welcome-screen
+            using BankContext context = new BankContext();
+
+            while (true)
+            {
+                Console.WriteLine("Welcome to BankNET!");
+                Console.WriteLine("Would you like to log in (y/n)?");
+                string wantToLogin = Console.ReadLine().ToLower();
+
+                if (wantToLogin == "y")
+                {
+                    Menus.Login(context);
+                }
+            }
+            
+           
+            // Add welcome-screen
 
            // Add login interface. Logging out from an account should bring you back here.
 
--- BankNET/Utilities/Menus.cs
+++ BankNET/Utilities/Menus.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using BankNET.Data;
+using BankNET.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,10 +12,107 @@ namespace BankNET.Utilities
     // Class containing methods that print the different menus.
     internal static class Menus
     {
-        // Add method for handling login screen.
+        
+        public static void Login(BankContext context)
+        {
+            bool tryAgainUserName = true;
+            bool tryAgainPin = true;
+            
+            Console.WriteLine("Log in");
 
-        // Add method for handling user menu.
+            while (tryAgainUserName)
+            {
+                Console.Write("Enter username: ");
+                string username = Console.ReadLine();
+
+                bool validUserName = context.Users.Any(uN => uN.UserName.Equals(username));
+            
+                if (validUserName)
+                {
+                    Console.Write("Enter pin: ");
+                    string pin = Console.ReadLine();
+
+                    bool validPin = context.Users.Any(p => p.Pin.Equals(pin));
+
+                    while (tryAgainPin)
+                    {
+                        if (validPin)
+                        {
+                            Console.WriteLine("Login Successfull!");
+                            tryAgainPin = false;
+                            
+                            if (username == "admin") AdminMenu();
+                            else UserMainMenu(context, username);
+                        }
+                        else Console.WriteLine("Wrong pin, try again.");
+                    }
+
+                    tryAgainUserName = false;
+                }
+                else Console.WriteLine("Invalid username, try again.");
+            }
+            
+        }
+        // Method for user Main Menu and handling menu
+        public static void UserMainMenu(BankContext context, string username)
+        {
+            Console.WriteLine("1. See your accounts and balance" +
+                "\n2. Transfer between accounts" +
+                "\n3. Withdrawal" +
+                "\n4. Deposit" +
+                "\n5. Open new account" +
+                "\n6. Log out");
+            Console.Write("What would you like to do: ");
+            int userMenuChoice = int.Parse(Console.ReadLine());
+            bool keepTryingUserMenu = true;
+
+            while (keepTryingUserMenu)
+            {
+                switch (userMenuChoice)
+                {
+                    case 1:
+                        ViewAccountBalance(context, username);
+                        break;
+                    case 2:
+                        TransferInternal(context, username);
+                        break;
+                    case 3:
+                        Withdraw(context, username);
+                        break;
+                    case 4:
+                        Deposit(context, username);
+                        break;
+                    case 5:
+                        CreateNewAccount(context, username);
+                        break;
+                    case 6:
+                        Console.Clear();
+                        Console.WriteLine("You are logged out.");
+                        keepTryingUserMenu = false;
+                        break;
+                    default:
+                        Console.Write("Invalid input, try again.");
+                        break;
+                }
+            }
+        }
 
         // Add method for handling admin menu.
+        public static void AdminMenu()
+        {
+            //int adminMenuChoice = int.Parse(Console.ReadLine());
+            //switch (adminMenuChoice)
+            //{
+            //    case 1: 
+            //        Console.WriteLine("Add new user: ");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("You have successfully logged out.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid input, try again.");
+            //        break;
+            //}
+        }
     }
 }
